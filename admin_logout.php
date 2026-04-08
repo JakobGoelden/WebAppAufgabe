d30b8d4a31dc34271d8c6575d02196895aed81d7
MaikBaumgaertner <ma.baumgaertner@proton.me>
2026-04-08 16:03:02 +0200

    Logout site
    Needed to logout user

---
+++ admin_logout.php
@@ -0,0 +1,15 @@
+<?php
+//need to start session to handle cookies
+session_start();
+
+//delete session variables
+// $_SESSION = array(); could be used too
+session_unset();
+
+//kill session and declare faulty session cookie (or delete) in browser
+session_destroy();
+
+//redirect to login page
+header("Location: auth.php");
+exit;
+?>
