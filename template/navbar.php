5db1952b23f979835e32fa9d6692fe3833f93981
Moritz <236601327+Moonnkk@users.noreply.github.com>
2026-04-08 14:27:22 +0200

    basic navbar structure

---
+++ template/navbar.php
@@ -0,0 +1,12 @@
+<?php
+?>
+
+<nav class="navbar">
+    <div class="nav-left">
+        <a href="index.php">WebAppName</a>
+    </div>
+
+    <div class="nav-right">
+        <button class="nav-button">Login</button>
+    </div>
+</nav>
