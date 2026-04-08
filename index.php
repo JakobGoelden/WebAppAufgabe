eede35e92985e923743b23641ae67d20647ffb2e
Moritz <236601327+Moonnkk@users.noreply.github.com>
2026-04-08 14:57:12 +0200

    bugfixes

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <title>WebApp Projekt</title>
     <link rel="stylesheet" href="./style/main.css">
-    <link rel="stylesheet" href="../style/navbar.css">
+    <link rel="stylesheet" href="./style/navbar.css">
 </head>
 <body>
     <?php include './template/navbar.php'; ?>
