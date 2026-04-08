c9fa6b9269b588b7b162ebee15832cee6ce9c789
Moritz <236601327+Moonnkk@users.noreply.github.com>
2026-04-08 13:57:00 +0200

    changes to index.php

--- index.php
+++ index.php
@@ -1,27 +1,22 @@
-<<<<<<< HEAD
 <?php
 ?>
 <!DOCTYPE html>
 <html lang="en">
-<!-- <meta http-equiv="refresh" content="5; URL=http://localhost/test.html" > -->
 <head>
     <meta charset="UTF-8">
     <title>WebApp Projekt</title>
-    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="./style/main.css">
 </head>
 <body>
     <?php include './template/navbar.php'; ?>
 
-
-    <!--<p style="display: flex; direction: rtl;" ><a class="admin" href="auth.php">Admin</a></p>-->
     <h1>MyProject.local is running</h1>
-  
 
     <p>Try <a class="underline">a PHP form</a> to test dynamic input.</p>
     <p>Try <a class="underline">Subsite</a> to test links</p>
     <br>
 
-  
+    <?php include './template/footer.php'; ?>
 </body>
 </html>
 
