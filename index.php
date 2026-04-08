ec004e6e84874d7fe48946e5535d6c314eb32cf8
Moritz <236601327+Moonnkk@users.noreply.github.com>
2026-04-08 15:06:23 +0200

    updated looks of navbar

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
-<!DOCTYPE html>
+<?php
+
+?><!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <title>WebApp Projekt</title>
     <link rel="stylesheet" href="./style/main.css">
-    <link rel="stylesheet" href="./style/navbar.css">
 </head>
 <body>
     <?php include './template/navbar.php'; ?>
--- template/navbar.php
+++ template/navbar.php
@@ -1,3 +1,7 @@
+<?php
+
+?>
+<link rel="stylesheet" href="./style/navbar.css">
 <nav class="navbar">
     <div class="nav-left">
         <a href="index.php">WebAppName</a>
