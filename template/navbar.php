535734b382a2f0695e959d7050e70f3094f8d0f0
Moritz <236601327+Moonnkk@users.noreply.github.com>
2026-04-09 15:40:12 +0200

    navbar style

--- template/navbar.php
+++ template/navbar.php
@@ -1,25 +1,25 @@
 <?php
 
 ?>
-<link rel="stylesheet" href="./style/navbar.css">
+<link rel="stylesheet" href="../style/navbar.css">
 <nav class="navbar">
     <div class="nav-left">
         <a href="index.php">WebAppName</a>
     </div>
 
-    <div class="subsite">
+    <div class="nav-subsite">
         <a href="index.php">Subsite1</a>
     </div>
 
-    <div class="subsite">
+    <div class="nav-subsite">
         <a href="index.php">Subsite2</a>
     </div>
 
-    <div class="subsite">
+    <div class="nav-subsite">
         <a href="index.php">Subsite3</a>
     </div>
 
-    <div class="subsite">
+    <div class="nav-subsite">
         <a href="index.php">Subsite4</a>
     </div>
 
