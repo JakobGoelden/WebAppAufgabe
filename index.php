a7042820e1c468495ef346c46b5663c9e1eb6d2a
Jakob Goelden <jakob@goelden.eu>
2026-04-10 17:34:32 +0200

    Side-by-Side divs

--- index.php
+++ index.php
@@ -14,7 +14,9 @@ require_once("init.php");?>
     <div name="Titel">
     </div>
 
-    <div name="Links">
+    <div name="Links" class="container">
+        <div name="feature1" class="subsite"></div>
+        <div name="feature2" class="subsite"></div>
     </div>
 
     <?php include './template/footer.php'; ?>
