97c4860633298fa05885eb610a5c00c87ea85a5f
Jakob Goelden <jakob@goelden.eu>
2026-05-06 17:00:16 +0200

    LMSIDHS

---
+++ fingerprinting/info.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>WebApp Projekt</title>
+    <link rel="stylesheet" href="../style/main.css">
+    <link rel="stylesheet" href="../style/navbar.css">
+</head>
+<body>
+    <?php include '../template/navbar.php'; ?>
+
+    
+</body>
